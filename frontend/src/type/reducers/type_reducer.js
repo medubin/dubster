7d6c7a0d6b87d00eddcddb31288570b137081b6a
Matt Dubin <medubin@gmail.com>
2017-12-03 15:29:26 -0800

    type reducer

---
+++ frontend/src/type/reducers/type_reducer.js
@@ -0,0 +1,22 @@
+import {RECEIVE_TYPE, RECEIVE_TYPES} from '../actions/type_actions';
+import merge from 'lodash/merge';
+
+const _nullTypes = Object.freeze({
+    types: []
+});
+
+const TypeReducer = (state = _nullTypes, action) => {
+    let newState = merge({}, state);
+    Object.freeze(state);
+    
+    switch(action.type) {
+        case RECEIVE_TYPES:
+            newState.types = action.types;
+            return newState;
+        case RECEIVE_TYPE:
+            newState.types = merge(newState.types, action.data);
+            return newState;
+        default:
+            return state;
+    }
+}
